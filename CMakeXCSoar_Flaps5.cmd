10c485be9daae61cfa26cab1a7253621e58e1625
aug <uaugustin@baumer.com>
2020-03-24 14:49:57 +0100

    work: - solve the cmake errors on PC0781 - project is with MSCV2019 compilabel and runable!

--- CMakeXCSoar_Flaps5.cmd
+++ CMakeXCSoar_Flaps5.cmd
@@ -8,9 +8,10 @@ set SOURCE_DIR=%CD%
 set BINARY_DIR=D:\Projects\Binaries
 set TARGET_PLATFORM=64
 set THIRD_PARTY=D:/Projects/3rd_Party
+set LINK_LIBS=D:\link_libs
 set COMPILER=VS2019
 set PROGRAM_DIR=D:\Programs
-set Boost_ROOT=D:\link_libs\boost\boost_1_72_0\vc142
-set QT_ROOT=%THIRD_PARTY%/qt
+set Boost_ROOT=%LINK_LIBS%\boost\boost_1_72_0\msvc2019
+set QT_ROOT=%THIRD_PARTY%\qt
 
 call CMakeXCSoar.cmd
